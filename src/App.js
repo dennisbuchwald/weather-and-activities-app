1ea368b0686a90a1891a3736f68cc0c172d4a124
Dennis Buchwald <buchwald95@icloud.com>
2023-03-08 13:12:29 +0100

    fix handleSubmit function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import { Form } from "./components/Form"
+import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
+  const handleAddActivity = (data) => console.log(data);
   return (
     <div>
-      <Form/>
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
-
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,46 @@
 import "./form.css";
-import { useState } from "react"
+import { useState } from "react";
 
-export function Form(onAddActivity){
+export function Form(onAddActivity) {
+  const [name, setName] = useState("");
+  const [isForGoodWeather, setIsForGoodWeather] = useState(false);
 
-    const [name, setName] = useState("")
-    const [isForGoodWeather, setIsForGoodWeather] = useState(false)
+  //keine Funktion wie müssen hier eine "const" mit dem wert einer Funktion vergeben
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-function handleSubmit = (event) => {
-event.preventDefault();
+    // Gibt den Namen und den isForGoodWeather-Wert aus den Zuständen aus
+    const data = { name, isForGoodWeather };
 
-const data = {name, isForGoodWeather}
+    // Rufe onAddActivity auf und übergebe das Datenobjekt als Argument
+    onAddActivity(data);
 
-}
+    // Setze den Namen-Zustand auf leer und den isForGoodWeather-Zustand auf false
+    setName("");
+    setIsForGoodWeather(false);
 
-    return (
+    // Setze den Fokus auf das erste input-Element
+    document.getElementById("name").focus();
+  };
 
+  return (
     <form onSubmit={handleSubmit}>
-    <h2>Add new Activity</h2>
-    <label className="name">Name:</label>
-    <input id="name" type="text" value={name} />
-    <br />
-    <label className="good-weather-activity">Good-weather activity:</label>
-    <input type="checkbox" id="good-weather-activity" checked={isForGoodWeather} />
-    <br />
-    <input type="submit" />
+      <h2>Add new Activity</h2>
+      <label className="name">Name:</label>
+      <input
+        id="name"
+        type="text"
+        onChange={(event) => setName(event.target.value)}
+      />
+      <br />
+      <label className="good-weather-activity">Good-weather activity:</label>
+      <input
+        type="checkbox"
+        id="good-weather-activity"
+        onChange={(event) => setIsForGoodWeather(event.target.value)}
+      />
+      <br />
+      <input type="submit" />
     </form>
-
-    );
+  );
 }
-
-
