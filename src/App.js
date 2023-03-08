9e608dde96029f9e8cd608da26331949258e8fe5
Dennis Buchwald <buchwald95@icloud.com>
2023-03-08 15:47:17 +0100

    create list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Form } from "./components/Form";
+import { Form } from "./components/Form/Form";
+import { List } from "./components/List/List";
 import { uid } from "uid";
 import { useState } from "react";
 
