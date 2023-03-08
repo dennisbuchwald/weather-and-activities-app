d7aa75e92d9981fa3e6050e457fbde614fe329d7
Melissa Liebeck <melissaliebeck@aol.com>
2023-03-08 11:52:49 +0100

    work on submit functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,27 @@
 import "./form.css";
+import { useState } from "react"
 
-export function Form(){
+export function Form(onAddActivity){
+
+    const [name, setName] = useState("")
+    const [isForGoodWeather, setIsForGoodWeather] = useState(false)
+
+function handleSubmit = (event) => {
+event.preventDefault();
+
+const data = {name, isForGoodWeather}
+
+}
+
     return (
 
-    <form>
+    <form onSubmit={handleSubmit}>
     <h2>Add new Activity</h2>
     <label className="name">Name:</label>
-    <input id="name" type="text" />
+    <input id="name" type="text" value={name} />
     <br />
     <label className="good-weather-activity">Good-weather activity:</label>
-    <input type="checkbox" id="good-weather-activity" />
+    <input type="checkbox" id="good-weather-activity" checked={isForGoodWeather} />
     <br />
     <input type="submit" />
     </form>
